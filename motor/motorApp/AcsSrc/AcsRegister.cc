1a3b55bf6ec0a48dcaad82cbf387e530e18ac786
Stephanie Allison <saa@slac.stanford.edu>
2004-07-16 19:10:39 +0000

    Motor version 5.4 from Ron Sluiter at APS: http://www.aps.anl.gov/upd/people/sluiter/epics/motor/index.html

--- motor/motorApp/AcsSrc/AcsRegister.cc
+++ motor/motorApp/AcsSrc/AcsRegister.cc
@@ -2,9 +2,9 @@
 FILENAME...	AcsRegister.cc
 USAGE...	Register ACS motor device driver shell commands.
 
-Version:	1.1
+Version:	1.4
 Modified By:	sluiter
-Last Modified:	2003/05/27 13:39:40
+Last Modified:	2004/07/16 19:06:58
 */
 
 /*****************************************************************
@@ -27,28 +27,24 @@ extern "C"
 
 // ACS Setup arguments
 static const iocshArg setupArg0 = {"Max. controller count", iocshArgInt};
-static const iocshArg setupArg1 = {"N/A", iocshArgInt};
-static const iocshArg setupArg2 = {"Polling rate", iocshArgInt};
+static const iocshArg setupArg1 = {"Polling rate", iocshArgInt};
 // ACS Config arguments
 static const iocshArg configArg0 = {"Card being configured", iocshArgInt};
-static const iocshArg configArg1 = {"MPF server location", iocshArgInt};
-static const iocshArg configArg2 = {"MPF server task name", iocshArgString};
+static const iocshArg configArg1 = {"asyn port name", iocshArgString};
 
-static const iocshArg * const MCB4BSetupArgs[3]  = {&setupArg0, &setupArg1,
-    &setupArg2};
-static const iocshArg * const MCB4BConfigArgs[3] = {&configArg0, &configArg1,
-    &configArg2};
+static const iocshArg * const MCB4BSetupArgs[2]  = {&setupArg0, &setupArg1};
+static const iocshArg * const MCB4BConfigArgs[2] = {&configArg0, &configArg1};
 
-static const iocshFuncDef setupMCB4B  = {"MCB4BSetup",  3, MCB4BSetupArgs};
-static const iocshFuncDef configMCB4B = {"MCB4BConfig", 3, MCB4BConfigArgs};
+static const iocshFuncDef setupMCB4B  = {"MCB4BSetup",  2, MCB4BSetupArgs};
+static const iocshFuncDef configMCB4B = {"MCB4BConfig", 2, MCB4BConfigArgs};
 
 static void setupMCB4BCallFunc(const iocshArgBuf *args)
 {
-    MCB4BSetup(args[0].ival, args[1].ival, args[2].ival);
+    MCB4BSetup(args[0].ival, args[1].ival);
 }
 static void configMCB4BCallFunc(const iocshArgBuf *args)
 {
-    MCB4BConfig(args[0].ival, args[1].ival, args[2].sval);
+    MCB4BConfig(args[0].ival, args[1].sval);
 }
 
 static void AcsRegister(void)
